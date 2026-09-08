d4e505a7887553bd977798c70b79ee09484f08a3
AJIT DEV <ajitk23192@gmail.com>
2026-09-08 09:33:14 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.38%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int ans = 0;
+        if (n < 1000)
+            return 0;
 
-        // 1,000 to n → 1 comma each
-        if (n >= 1000) {
-            ans += n - 999;
-        }
-
-        // 1,000,000+ would have 2 commas,
-        // but n <= 10^5, so this case never occurs.
-
-        return ans;
+        return n - 999;
     }
 };
