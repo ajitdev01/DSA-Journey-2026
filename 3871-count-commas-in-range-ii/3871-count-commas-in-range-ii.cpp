e2c2fa47ea6b23daa9b239be5f9b7662bbaaf892
AJIT DEV <ajitk23192@gmail.com>
2026-09-10 15:45:34 +0530

    Time: 0 ms (100%), Space: 8.9 MB (99.4%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -2,31 +2,16 @@ class Solution {
 public:
     long long countCommas(long long n) {
         long long ans = 0;
+        long long start = 1000;
+        long long commas = 1;
 
-        // 1,000 to 999,999
-        if (n >= 1000) {
-            ans += min(n, 999999LL) - 1000 + 1;
-        }
+        while (start <= n) {
+            long long end = min(n, start * 1000 - 1);
 
-        // 1,000,000 to 999,999,999
-        if (n >= 1000000) {
-            ans += 2LL * (min(n, 999999999LL) - 1000000 + 1);
-        }
+            ans += (end - start + 1) * commas;
 
-        // 1,000,000,000 to 999,999,999,999
-        if (n >= 1000000000LL) {
-            ans += 3LL * (min(n, 999999999999LL) - 1000000000LL + 1);
-        }
-
-        // 1,000,000,000,000 to 999,999,999,999,999
-        if (n >= 1000000000000LL) {
-            ans += 4LL * (min(n, 999999999999999LL)
-                         - 1000000000000LL + 1);
-        }
-
-        // 1,000,000,000,000,000 to n
-        if (n >= 1000000000000000LL) {
-            ans += 5LL * (n - 1000000000000000LL + 1);
+            start *= 1000;
+            commas++;
         }
 
         return ans;
