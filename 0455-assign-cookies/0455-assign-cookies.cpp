1650c15e580b1014b824b3b0142ba862ad2cb386
AJIT DEV <ajitk23192@gmail.com>
2026-09-22 11:00:34 +0530

    Time: 2 ms (96.18%), Space: 44.6 MB (97.19%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    int findContentChildren(vector<int>& student, vector<int>& cookie) {
-        sort(student.begin(), student.end());
-        sort(cookie.begin(), cookie.end());
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
 
-        int studentPtr = 0;
+        int i = 0;
 
-        for (int cookiePtr = 0;
-             studentPtr < student.size() && cookiePtr < cookie.size();
-             ++cookiePtr) {
-
-            if (cookie[cookiePtr] >= student[studentPtr]) {
-                ++studentPtr;
+        for (int j = 0; i < g.size() && j < s.size(); ++j) {
+            if (s[j] >= g[i]) {
+                ++i;
             }
         }
 
-        return studentPtr;
+        return i;
     }
 };
